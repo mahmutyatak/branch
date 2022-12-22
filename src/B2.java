f36af46f056f0468148d1ce9372a3720cb80ef28
mahmut yatak <alexmahone2324@gmail.com>
2022-12-22 13:51:49 +0300

    v2

---
+++ src/B2.java
@@ -0,0 +1,22 @@
+public class B2 {
+    public static void main(String[] args) {
+        /*
+        temelde git ve git hub için 3 aşamadan bahsedebiliriz
+        1- projemiz veya klasörümüz ile giti tanıştırmak
+                git init
+        2- projemiz veya klasörümüz için versiyon oluşturma
+        git add.
+        git commit -m ""
+        3- git hub ile senkronizasyon
+
+        --- ilk defa entegre etmek için önce github da yeni repo oluşturalım
+        oradaki son 3 adımı kopyalayıp terminale yapıştırıp enter
+
+        ---- ilk entegrasyondan sonra her değişiklik yaptığımızda
+            -git  add .
+            git commit -m ""
+            git push
+
+         */
+    }
+}
