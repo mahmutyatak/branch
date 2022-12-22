45b9b07426fc7cea26a5e3b5f65cd35161e79bcb
mahmut yatak <alexmahone2324@gmail.com>
2022-12-22 14:13:00 +0300

    v3

---
+++ src/B3.java
@@ -0,0 +1,16 @@
+public class B3 {
+    public static void main(String[] args) {
+        /*
+        projemizde önemli bir değişiklik yapmak istiyoruz
+        bu değişikliğe 10 gün çalışacağız.
+        değişikliği yapmaya devam ederken orjinal projemizi riske atmamk
+        ve eğer son kullanıcı kullanıyorsa oluşabilecek sorunları engellemek için
+        orjinal proje üzerinde çalışmak yerne
+        yeni bir branch oluşturup
+        o brnach de değişiklikleri yaparız
+        çalışma sonucunda istenen hedefe ulaşabilirsek
+        oluşturduğumuz branch i asıl branch e kopyalarız.
+
+         */
+    }
+}
